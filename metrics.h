9bfc648b24b33653ccdb572d238be4b04d328f9e
vcornejov <vcornejv@gmail.com>
2025-04-05 12:24:33 -0300

    Update metrics.h

--- metrics.h
+++ metrics.h
@@ -1,1 +1,10 @@
+#ifndef METRICS_H
+#define METRICS_H
+
+#include "order.h"
+
+char* calculate_metric(const char *metric_name, struct order *orders, int size);
+char* pizza_mas_vendida(struct order *orders, int size);
+
+#endif // METRICS_H
 
